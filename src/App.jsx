fe60987a4dab9fca6300205220abd6cee7cc6760
mikofs13 <141562481+mikofs13@users.noreply.github.com>
2024-01-16 21:34:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
+import axios from "axios"
+
+
 const App = () => {
+
+  axios.defaults.withCredentials = true;
   return (
     <>
       <Header />
